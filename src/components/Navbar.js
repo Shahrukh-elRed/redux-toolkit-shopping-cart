16dccc5223877c8d9d537af7e5c497b966b05312
Shahrukh <shahrukh1494@gmail.com>
2024-03-08 17:39:33 +0530

    fetch and show products from fakestore api

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div style={{ display: "flex", alignItems: "space-between" }}>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
       <span className="logo">REDUX TOOLKIT STORE</span>
       <div>
         <Link className="navLink" to="/">
