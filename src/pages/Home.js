00bdb32826f206bdaaf81d475211cdaef803e39f
Shahrukh <shahrukh1494@gmail.com>
2024-03-08 11:15:46 +0530

    redux toolkit shopping cart demo project - first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="heading">Welcome to Redux Toolkit Store</h2>
+      <section>
+        <h3>Products</h3>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
